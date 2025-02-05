d9c85f74aa624e381f0bec1881662f71d3c06756
Tomas025 <tomasbraz43@hotmail.com>
2025-02-05 11:58:05 -0300

    Requisições com HttpClient

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Pensamento } from './pensamento'
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class PensamentoService {
 
-  constructor() { }
+  private readonly API = 'http://localhost:3000/pensamentos'
+
+  constructor(private http: HttpClient) { }
+
+  listar() {
+    return this.http.get<Pensamento[]>(this.API)
+  }
+
 }
